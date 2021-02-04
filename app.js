d52de1737349201fe1d15d530a9abfc9ff3128d1
Tahreem <syedatahreem.sta@gmail.com>
2021-02-04 15:13:19 +1100

    Second Commit

--- app.js
+++ app.js
@@ -28,12 +28,13 @@ const baseUI = document.querySelector('#base'),
 
 base.addEventListener('input', (event) =>{
     baseVal = event.target.value
+    baseVal = baseVal.toUpperCase()
     
 })
 
 term.addEventListener('input', (event) =>{
     termVal = event.target.value
-    
+    termVal = termVal.toUpperCase()
 })
 
 amount.addEventListener('input', (event) =>{
@@ -43,18 +44,36 @@ amount.addEventListener('input', (event) =>{
 
 convertBtn.addEventListener('click', () => {
     let result = calculateConverter(baseVal, termVal, amountVal);
-    if(termVal==="JPY")
+    
+    //If conversion failed due to wrong input
+    if (result === -1) { 
+        color = "red"
+        setMessage(`Unable to find rates for ${baseVal} /  ${termVal}`, color )
+    }
+    //if conversion succesful
+    else {
+        if(termVal==="JPY")
     {
 
         result = result.toFixed(0);
     }
     else
     {
+         
         result= result.toFixed(2);
     }
-    setMessage(`result is ${result}`, color)
+        setMessage(`${termVal} ${result}`, color)
+
+    }
+
+         baseUI.value= ''
+         termUI.value=''
+         amountUI.value=0
+         color ="green"
+        
 })
 
+ 
 
 // Display result
 
@@ -67,38 +86,95 @@ function setMessage(msg, color){
 
 function calculateConverter(base,term,amount){
 
+    let newAmount, reference;
+
     for(var i in myObjects.coCurr)
+
     {
+        // Base to Term exists
        if( myObjects.coCurr[i].base=== base && term === myObjects.coCurr[i].term)
         {
             newAmount = myObjects.coCurr[i].rate * amount;
             return newAmount;
         }
+
+        // Base to Term inverted exists
         else if( myObjects.coCurr[i].base=== term && myObjects.coCurr[i].term=== base)
         {
-            newAmount = 1 / myObjects.coCurr[i].rate *amount;
+            newAmount = (1 / myObjects.coCurr[i].rate )*amount;
             return newAmount;
         }
-        else if(myObjects.coCurr[i].base ===base &&  term ===myObjects.coCurr[i].base)
+
+        // Base = Term
+        else if(base===term)
         {
-                 newAmount = amount;
-                 return newAmount;
+            newAmount = 1* amount;
+            return newAmount;
         }
+
+        //Base to reference and Reference to Term exists.
         else if(base ===myObjects.coCurr[i].base && term!= myObjects.coCurr[i].term)
          {
+            
             for ( var j in myObjects.coCurr)
             {
                 if(term === myObjects.coCurr[j].term )
                 {
+                     
                     if(myObjects.coCurr[j].base=== myObjects.coCurr[i].term)
                     {
+                      
+                        reference = myObjects.coCurr[j].base;
                         newAmount = myObjects.coCurr[i].rate * myObjects.coCurr[j].rate * amount;
                         return newAmount;
+                    }
+                    
+                }
+                else if( term=== myObjects.coCurr[j].base)
+                {
+                     if(myObjects.coCurr[i].term === myObjects.coCurr[j].term)
+                    {
+                        console.log("yes 4")
+                         reference = myObjects.coCurr[j].term
+                         newAmount = (myObjects.coCurr[i].rate / myObjects.coCurr[j].rate) * amount;
+                         return newAmount;
                     }
                 }
             }
         }
-        else if(base !=myObjects.coCurr[i].base && term=== myObjects.coCurr[i].term)
+
+        //Reference to Base and Term to Reference exists.
+        else if(base ===myObjects.coCurr[i].term && term !== myObjects.coCurr[i].base)
+        {
+           for ( var j in myObjects.coCurr)
+           {
+               if(term === myObjects.coCurr[j].base )
+               {
+                   if(myObjects.coCurr[j].term=== myObjects.coCurr[i].base)
+                   {
+                        reference = myObjects.coCurr[j].term
+                        newAmount = (1 / myObjects.coCurr[i].rate * myObjects.coCurr[j].rate) * amount;
+                        return newAmount;
+                   }
+               }
+               else if(term === myObjects.coCurr[j].term ) 
+              
+               {
+                   if (myObjects.coCurr[j].base === myObjects.coCurr[i].base)
+                   {
+                      reference = myObjects.coCurr[j].base
+                      newAmount = (1/ myObjects.coCurr[i].rate) * myObjects.coCurr[j].rate * amount;
+                      return newAmount;
+  
+                   }
+                      
+               }
+               
+           }
+       }
+
+
+     else if(base !=myObjects.coCurr[i].base && term=== myObjects.coCurr[i].term)
         {
            for ( var j in myObjects.coCurr)
            {
@@ -106,18 +182,31 @@ function calculateConverter(base,term,amount){
                {
                    if(myObjects.coCurr[j].base=== myObjects.coCurr[i].base)
                    {
-                       newAmount = myObjects.coCurr[i].rate * myObjects.coCurr[j].rate * amount;
+                       newAmount = (myObjects.coCurr[i].rate / myObjects.coCurr[j].rate) * amount;
                        return newAmount;
                    }
                }
            }
        }
-        
-            
-
-    
+ 
+   }
     }
-}
+    
+    // for(var i in myObjects.coCurr){
+    // for ( var j in myObjects.coCurr){
+    //     if(base!= myObjects.coCurr[i].base && term!= myObjects.coCurr[j].term
+    //        && term!= myObjects.coCurr[i].base && base!= myObjects.coCurr[j].term)
+    //    {
+    //         return -1;
+           
+    //    }
+       
+    //   }
+
+    // }
+
+
+
     
 
 
